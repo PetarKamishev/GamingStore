8826ded4166ff8bd33389ba3a0ab9a8a09fa99e8
PetarKamishev <petar.kamishev01@gmail.com>
2024-08-08 15:20:31 +0300

    Added more games to InMemoryDb

--- GamingStore.DL/InMemoryDb/InMemoryDb.cs
+++ GamingStore.DL/InMemoryDb/InMemoryDb.cs
@@ -12,11 +12,27 @@ namespace GamingStore.GamingStore.DL.InMemoryDb
                 Id = 1,
                 Title = "BattleFront 2",
                 ReleaseDate = new DateTime(2017, 11, 17),
-                GameTags= new string[] {"FPS", "Shooter", "SinglePlayer", "Multiplayer", "Action"},
+                GameTags= new string[] {"FPS", "Shooter", "SinglePlayer", "MultiPlayer", "Action"},
                 Price=9.99M
+            },
+
+            new Games()
+            {
+                Id=2,
+                Title = "League of Legends",
+                ReleaseDate = new DateTime(2009, 10, 27),
+                GameTags= new string[] {"Moba", "MultiPlayer"},
+                Price=0M
+            },
+
+            new Games()
+            {
+                Id=3,
+                Title="Hogwarts Legacy",
+                ReleaseDate = new DateTime(2023, 02, 10),
+                GameTags= new string[] {"Puzzle", "SinglePlayer", "Action", "RPG", "Adventure"},
+                Price=59.99M
             }
-
-
         };
     }
 }
